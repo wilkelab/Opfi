db72652718742c58db997e87ad4c457c1b184873
Alexis Hill <alexismhill3@gmail.com>
2020-06-06 16:28:47 -0600

    change filenames

---
+++ src/crisposon/option_handling.py
@@ -0,0 +1,5 @@
+from crisposon.blast_options import (BLAST_OPTIONS_COMMON,
+                                        BLAST_FLAGS,
+                                        BLASTP_OPTIONS,
+                                        PSIBLAST_OPTIONS)
+
