63df050ffa665e405dfdcf1df209fbb8ff4cb5b5
Alexis Hill <alexismhill3@gmail.com>
2020-06-07 12:50:39 -0600

    actually won't need this file

--- src/crisposon/option_handling.py
+++
@@ -1,5 +0,0 @@
-from crisposon.blast_options import (BLAST_OPTIONS_COMMON,
-                                        BLAST_FLAGS,
-                                        BLASTP_OPTIONS,
-                                        PSIBLAST_OPTIONS)
-
