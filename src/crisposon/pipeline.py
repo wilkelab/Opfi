cd2dfe5f429f76f58798e71b2191d8f3f6d04a87
Alexis Hill <alexismhill3@gmail.com>
2020-07-06 09:19:42 -0600

    save input data path in output, rather than just the file name

--- src/crisposon/pipeline.py
+++ src/crisposon/pipeline.py
@@ -390,9 +390,8 @@ class Pipeline:
                         json.dump(self._results, jsonfile)
 
             elif outfrmt == "CSV":
-                input_file_name = os.path.basename(self.data_path)
                 csv_writer = CSVWriter(self._results, outfile)
-                csv_writer.to_csv(input_file_name)
+                csv_writer.to_csv(self.data_path)
     
     
     def _record_all_hits(self, outfile):
