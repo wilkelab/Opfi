42c34bb34a263c35c27acfd3b45d20b6ff4423be
Alexis Hill <alexismhill3@gmail.com>
2020-08-06 16:17:40 -0600

    add tests for seed_with_coordinates_step

---
+++ tests/test_gene_finder/test_pipeline.py
@@ -0,0 +1,28 @@
+from gene_finder.pipeline import Pipeline
+
+
+def test_seed_with_coordinates_step1():
+    # use fasta2 and the cas.prot database from e2e tests
+    data = "tests/integration/integration_data/end-to-end/fasta2.fasta"
+    db = "tests/integration/integration_data/end-to-end/cas.prot"
+    p = Pipeline()
+    p.add_seed_with_coordinates_step(db=db, name="test1", e_val=0.001, blast_type="PROT")
+    results = p.run(data=data)
+    assert(len(results) == 2)
+    assert "Loc_0-14213" in results["fasta2_1"]
+    genes_names = [hit["Hit_name"] for hit in results["fasta2_1"]["Loc_0-14213"]["Hits"].values()]
+    assert "cas9" in genes_names
+
+
+def test_seed_with_coordinates_step2():
+    # use fasta2 and the cas.prot database from e2e tests
+    data = "tests/integration/integration_data/end-to-end/fasta2.fasta"
+    db = "tests/integration/integration_data/end-to-end/cas.prot"
+    p = Pipeline()
+    p.add_seed_with_coordinates_step(db=db, name="test1", e_val=0.001, blast_type="PROT", start=100, end=14000, contig_id="fasta2_1")
+    results = p.run(data=data)
+    assert(len(results) == 1)
+    assert "Loc_100-14000" in results["fasta2_1"]
+    genes_names = [hit["Hit_name"] for hit in results["fasta2_1"]["Loc_100-14000"]["Hits"].values()]
+    assert "cas9" in genes_names
+    
