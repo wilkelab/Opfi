434de3c2f286a0bc6c4306b6a6b169059b8196b4
Alexis Hill <alexismhill3@gmail.com>
2020-08-25 18:14:54 -0600

    write test output to a separate temporary directory instead of the root directory

--- tests/integration/test_gene_finder.py
+++ tests/integration/test_gene_finder.py
@@ -23,6 +23,7 @@ def create_pipeline(conf_file):
             p.add_crispr_step()
     return p
 
+
 def merge_data():
     """
     Temporarily merge multiple sequences from
@@ -44,8 +45,15 @@ def merge_data():
     return (tmp, merge)
 
 
+@pytest.fixture()
+def temporary_directory():
+    tmp = tempfile.TemporaryDirectory()
+    yield tmp
+    tmp.cleanup()
+    
+
 @pytest.mark.slow
-def test_with_blast():
+def test_with_blast(temporary_directory):
     """Test pipeline using BLAST as the alignment tool.
     
     Use --runslow option to run from command line. 
@@ -56,16 +64,16 @@ def test_with_blast():
     conf_file = "tests/integration/configs/blast_integration_test.yaml"
     p = create_pipeline(conf_file)
     
-    results = p.run(data=genomic_data)
+    results = p.run(data=genomic_data, output_directory=temporary_directory.name)
     
     assert len(results) == 1
     hits = results["NZ_CCKB01000071.1"]["Loc_78093-114093"]["Hits"]
     assert len(hits) == 11
     assert "Array_0" in hits
-    os.remove("gene_finder_results.csv")
+
 
 @pytest.mark.slow
-def test_multi_seq_fasta():
+def test_multi_seq_fasta(temporary_directory):
     """
     Test that the pipeline runs and produces the
     correct output for a multi-sequence fasta file.
@@ -78,15 +86,14 @@ def test_multi_seq_fasta():
     conf_file = "tests/integration/configs/blast_integration_test.yaml"
     p = create_pipeline(conf_file)
     
-    results = p.run(data=genomic_data)
+    results = p.run(data=genomic_data, output_directory=temporary_directory.name)
     
     assert len(results) == 2
     tmp.cleanup()
-    os.remove("gene_finder_results.csv")
 
 
 @pytest.mark.slow
-def test_gzip_fasta():
+def test_gzip_fasta(temporary_directory):
     """
     Test that the pipeline can open and correctly process
     a gzipped fasta file.
@@ -95,16 +102,15 @@ def test_gzip_fasta():
     data = "tests/integration/integration_data/contigs/record_all_hits_test_1.gz"
     conf_file = "tests/integration/configs/blast_integration_test.yaml"
     p = create_pipeline(conf_file)
-    results = p.run(data=data, gzip=True)
+    results = p.run(data=data, gzip=True, output_directory=temporary_directory.name)
 
     hits = results["KB405063.1"]["Loc_0-23815"]["Hits"]
     assert "Cas_all_hit-0" in hits
     assert "Array_0" in hits
-    os.remove("gene_finder_results.csv")
     
 
 @pytest.mark.slow
-def test_record_all_hits_1():
+def test_record_all_hits_1(temporary_directory):
     """
     Testcase: input contig contains a putative
     CRISPR-Transposon system (tnsAB/cas genes/CRISPR array - note no tnsC/D though).
@@ -119,20 +125,18 @@ def test_record_all_hits_1():
     conf_file = "tests/integration/configs/blast_integration_test.yaml"
     p = create_pipeline(conf_file)
 
-    p.run(data=genomic_data, record_all_hits=True)
+    p.run(data=genomic_data, record_all_hits=True, output_directory=temporary_directory.name)
 
-    with open("gene_finder_hits.json", "r") as f:
+    with open(os.path.join(temporary_directory.name, "gene_finder_hits.json"), "r") as f:
         hits = json.load(f)["KB405063.1"]["hits"]
         assert len(hits) == 3
         assert "tnsAB" in hits
         assert "cas_all" in hits
         assert "CRISPR" in hits
-    os.remove("gene_finder_results.csv")
-    os.remove("gene_finder_hits.json")
 
 
 @pytest.mark.slow
-def test_record_all_hits_2():
+def test_record_all_hits_2(temporary_directory):
     """
     Testcase: input contig has a hit for tnsA 
     but no hits for cas genes.
@@ -142,21 +146,19 @@ def test_record_all_hits_2():
     conf_file = "tests/integration/configs/blast_integration_test.yaml"
     p = create_pipeline(conf_file)
 
-    p.run(data=genomic_data, record_all_hits=True)
+    p.run(data=genomic_data, record_all_hits=True, output_directory=temporary_directory.name)
 
-    with open("gene_finder_hits.json", "r") as f:
+    with open(os.path.join(temporary_directory.name, "gene_finder_hits.json"), "r") as f:
         hits = json.load(f)["JNYE01000019.1"]["hits"]
         assert len(hits) == 1
         assert "tnsAB" in hits
         assert "cas_all" not in hits
         assert "tnsCD" not in hits
         assert "CRISPR" not in hits
-    os.remove("gene_finder_hits.json")
-    os.remove("gene_finder_results.csv")
 
 
 @pytest.mark.slow
-def test_pipeline_with_arbitrary_blast_flags1():
+def test_pipeline_with_arbitrary_blast_flags1(temporary_directory):
     """Tries to run the pipeline using arbitrary blast flags at each step."""
 
     genomic_data = "tests/integration/integration_data/contigs/record_all_hits_test_1"
@@ -167,21 +169,19 @@ def test_pipeline_with_arbitrary_blast_flags1():
     p.add_seed_step(db=tnsAB_db, name="tnsAB", e_val=0.001, blast_type="PROT", word_size=4, gap_open=12)
     p.add_blast_step(db=tns_CD, name="tnsCD", e_val=0.001, blast_type="PSI", gap_trigger=24)
     p.add_filter_step(db=cas_db, name="cas", e_val=0.001, blast_type="PROT", lcase_masking=True)
-    results = p.run(data=genomic_data)
+    results = p.run(data=genomic_data, output_directory=temporary_directory.name)
     # the test data should contain some genes of interest; check that we still found something
     assert len(results) != 0
-    os.remove("gene_finder_results.csv")
 
 
 @pytest.mark.slow
-def test_pipeline_with_arbitrary_blast_flags2():
+def test_pipeline_with_arbitrary_blast_flags2(temporary_directory):
     """Tries to run the pipeline using arbitrary blast flags at each step."""
 
     genomic_data = "tests/integration/integration_data/contigs/record_all_hits_test_1"
     tnsAB_db = "tests/integration/integration_data/blast_databases/tnsAB/blast_db"
     p = Pipeline()
     p.add_seed_with_coordinates_step(db=tnsAB_db, name="tnsAB", e_val=0.001, blast_type="PROT", word_size=4, gap_open=12)
-    results = p.run(data=genomic_data)
+    results = p.run(data=genomic_data, output_directory=temporary_directory.name)
     # the test data should contain some genes of interest; check that we still found something
     assert len(results) != 0
-    os.remove("gene_finder_results.csv")
