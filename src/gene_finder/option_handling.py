0282405beb96c002a6cb7f2f32fcfbd620260834
Jim Rybarski <jim@rybarski.com>
2020-11-08 14:53:36 -0600

    Allow user to set path to BLAST
    Users can choose the path to the executable they want to use. If no
    selection is made, it will default to 'blastp', 'blastn', or 'psiblast',
    as appropriate. This will make it easy to use blastp or blastn v2.10 on
    our cluster.

--- src/gene_finder/option_handling.py
+++ src/gene_finder/option_handling.py
@@ -21,7 +21,7 @@ PSIBLAST_OPTIONS = ['gap_trigger', 'num_iterations', 'out_pssm',
 BLASTN_OPTIONS = ['filtering_algorithm', 'sum_stats', 'window_masker_db', 'window_size', 'template_type', 'version', 'parse_deflines', 'min_raw_gapped_score', 'string', 'format', 'max_hsps', 'taxids', 'negative_taxids', 'num_alignments', 'strand', 'off_diagonal_range', 'subject_besthit', 'num_sequences', 'no_greedy', 'negative_taxidlist', 'culling_limit', 'xdrop_ungap', 'open_penalty', 'DUST_options', 'sorthits', 'xdrop_gap_final', 'negative_gilist', 'subject', 'use_index', 'bool_value', 'filename', 'seqidlist', 'task_name', 'sort_hits', 'database_name', 'lcase_masking', 'query_loc', 'subject_loc', 'sort_hsps', 'line_length', 'boolean', 'db_hard_mask', 'negative_seqidlist', 'template_length', 'filtering_db', 'filtering_database', 'penalty', 'searchsp', 'ungapped', 'type', 'gapextend', 'db_soft_mask', 'dbsize', 'qcov_hsp_perc', 'sorthsps', 'window_masker_taxid', 'index_name', 'export_search_strategy', 'float_value', 'soft_masking', 'gilist', 'entrez_query', 'show_gis', 'best_hit_score_edge', 'gapopen', 'subject_input_file', 'range', 'html', 'word_size', 'best_hit_overhang', 'perc_identity', 'input_file', 'num_descriptions', 'xdrop_gap', 'dust', 'taxidlist', 'max_target_seqs', 'num_threads', 'task', 'remote', 'int_value', 'extend_penalty', 'reward', 'import_search_strategy', 'num_letters']
 
 
-def build_blastp_command(query, db, evalue, kwargs, output_fields, out):
+def build_blastp_command(query, db, evalue, kwargs, output_fields, out, blastp_path):
     """
     Generate a python subprocess command (list) for
     executing blastp.
@@ -30,7 +30,7 @@ def build_blastp_command(query, db, evalue, kwargs, output_fields, out):
     at this time, only certain options are allowed.
     """
 
-    cmd = ["blastp", "-query", query, "-db", db, "-evalue", str(evalue), "-out", out]
+    cmd = [blastp_path, "-query", query, "-db", db, "-evalue", str(evalue), "-out", out]
     for key, value in kwargs.items():
         if key in (BLAST_OPTIONS_COMMON + BLASTP_OPTIONS):
             cmd.append("-{}".format(key))
@@ -42,7 +42,7 @@ def build_blastp_command(query, db, evalue, kwargs, output_fields, out):
     cmd.append("6 {}".format(output_fields))
     return cmd
 
-def build_psiblast_command(query, db, evalue, kwargs, output_fields, out):
+def build_psiblast_command(query, db, evalue, kwargs, output_fields, out, psiblast_path):
     """
     Generate a python subprocess command (list) for
     executing psiblast.
@@ -51,7 +51,7 @@ def build_psiblast_command(query, db, evalue, kwargs, output_fields, out):
     at this time, only certain options are allowed.
     """
 
-    cmd = ["psiblast", "-query", query, "-db", db, "-evalue", str(evalue), "-out", out]
+    cmd = [psiblast_path, "-query", query, "-db", db, "-evalue", str(evalue), "-out", out]
     for key, value in kwargs.items():
         if key in (BLAST_OPTIONS_COMMON + PSIBLAST_OPTIONS):
             cmd.append("-{}".format(key))
@@ -63,7 +63,7 @@ def build_psiblast_command(query, db, evalue, kwargs, output_fields, out):
     cmd.append("6 {}".format(output_fields))
     return cmd
 
-def build_blastn_command(query, db, evalue, kwargs, output_fields, out):
+def build_blastn_command(query, db, evalue, kwargs, output_fields, out, blastn_path):
     """
     Generate a python subprocess command (list) for
     executing blastn.
@@ -71,7 +71,7 @@ def build_blastn_command(query, db, evalue, kwargs, output_fields, out):
     kwargs should be valid blastn options. 
     """
 
-    cmd = ["blastn", "-query", query, "-db", db, "-evalue", str(evalue), "-out", out]
+    cmd = [blastn_path, "-query", query, "-db", db, "-evalue", str(evalue), "-out", out]
     for key, value in kwargs.items():
         if key in BLASTN_OPTIONS:
             cmd.append("-{}".format(key))
--- tests/test_gene_finder/test_options.py
+++ tests/test_gene_finder/test_options.py
@@ -4,7 +4,7 @@ def test_build_command1():
     # simply tests that the subprocess argument list is being constructed as expected
     kwargs = {"num_threads": 4, "ungapped": True, "qcov_hsp_perc": 90}
     cmd = build_blastp_command(query="my_query", db="my_db", evalue=0.001, kwargs=kwargs, 
-                               output_fields="qseqid sseqid stitle", out="out.tsv")
+                               output_fields="qseqid sseqid stitle", out="out.tsv", blastp_path='blastp')
     assert cmd == ["blastp", "-query", "my_query", "-db", "my_db", "-evalue", "0.001", "-out", "out.tsv",
                    "-num_threads", "4", "-ungapped", "-qcov_hsp_perc", "90", "-outfmt", "6 qseqid sseqid stitle"]
 
@@ -13,7 +13,7 @@ def test_build_command2():
     # tests that unknown/prohibited arguments are ignored
     kwargs = {"not_a_real": "blast_argument", "outfmt": 2}
     cmd = build_blastp_command(query="my_query", db="my_db", evalue=0.001, kwargs=kwargs, 
-                               output_fields="qseqid sseqid stitle", out="out.tsv")
+                               output_fields="qseqid sseqid stitle", out="out.tsv", blastp_path='blastp')
     assert cmd == ["blastp", "-query", "my_query", "-db", "my_db", "-evalue", "0.001", "-out", "out.tsv",
                    "-outfmt", "6 qseqid sseqid stitle"]
 
@@ -22,6 +22,6 @@ def test_build_command3():
     # test the psi blast version of this, for good measure
     kwargs = {"num_iterations": 3, "gap_trigger": 24}
     cmd = build_psiblast_command(query="my_query", db="my_db", evalue=0.001, kwargs=kwargs, 
-                               output_fields="qseqid sseqid stitle", out="out.tsv")
+                               output_fields="qseqid sseqid stitle", out="out.tsv", psiblast_path='psiblast')
     assert cmd == ["psiblast", "-query", "my_query", "-db", "my_db", "-evalue", "0.001", "-out", "out.tsv",
                    "-num_iterations", "3", "-gap_trigger", "24", "-outfmt", "6 qseqid sseqid stitle"]
